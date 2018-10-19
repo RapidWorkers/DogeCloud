39f7d2119502bc47dc67580799434bff8bd35085
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2018-10-19 14:27:22 +0900

    181019

--- DC_Relay_Server/Main.c
+++ DC_Relay_Server/Main.c
@@ -1,7 +1,56 @@
 #include <stdio.h>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define DEVELOPEMENT_MODE
+
+//FOR TESTING ONLY
+#ifdef DEVELOPEMENT_MODE
+#define BUF_SIZE 2048
+#define QUEUE_SIZE 10
+#define BIND_ADDR "127.0.0.1"
+#define BIND_PORT 15754
+#endif
 
 int main()
 {
+
+	WSADATA wsaData;
+	SOCKET hServSock, hClntSock;
+	SOCKADDR_IN servAddr, clntAddr;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		printf("Error: Init Sock fail");
+
+	hServSock = socket(PF_INET, SOCK_STREAM, 0);
+	if (hServSock == INVALID_SOCKET)
+		printf("Error: Invalid Sock");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servAddr.sin_port = htons(BIND_PORT);
+
+	if (bind(hServSock, &servAddr, sizeof(servAddr)))
+		printf("Error: Bind Fail");
+
+	if (listen(hServSock, 5) == SOCKET_ERROR)
+		printf("Error: Listen Fail");
+
+	int szClntAddr = sizeof(clntAddr);
+	hClntSock = accept(hServSock, (SOCKADDR*)&clntAddr, &szClntAddr);
+
+	if(hClntSock == INVALID_SOCKET)
+		printf("Error: Accept Fail");
+
+	send(hClntSock, "Hello World!", 13, 0);
+	closesocket(hClntSock);
+	closesocket(hServSock);
+	WSACleanup();
+
+	return 0;
+
 	printf("Not ready yet :) \n");
 	system("pause");
 }
--- DogeCloud/testDll.c
+++ DogeCloud/testDll.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
 #include "../DC_Common/DC_Common.h"
-#pragma comment(lib, "DC_Common.lib")
 
+#pragma comment(lib, "DC_Common.lib")
+#pragma comment(lib, "ws2_32.lib")
 
 int main() {
 	printf("============ 1. DLL Call Test ============\n");
@@ -36,6 +38,39 @@ int main() {
 
 	printf("ORG: opCode: %d clientVersion: %d struct_addr: %p\n", test.opCode, test.clientVersion, &test);
 	printf("RST: opCode: %d clientVersion: %d struct_addr: %p\n", temp.opCode, temp.clientVersion, &temp);
+
+	printf("\n\n============ 6. Server Connection Test ============\n");
+	WSADATA wsaData;
+	SOCKET hSocket;
+	SOCKADDR_IN servAddr;
+
+	char message[30];
+	int strLen;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) //소켓 라이브러리를 초기화하고 있다
+		printf("WSAStartup() error!\n");
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
+		printf("socket() error\n");
+
+	memset(&servAddr, 0, sizeof(servAddr));
+	servAddr.sin_family = AF_INET;
+	inet_pton(AF_INET, "127.0.0.1", &servAddr.sin_addr.s_addr);
+	servAddr.sin_port = htons(15754);
+
+	int err = (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR);
+	if (err) //생성된 소켓을 바탕으로 서버에 연결요청을 하고 있다
+	{
+		printf("connect() error!\n");
+		printf("code : %d", WSAGetLastError());
+	}
+	strLen = recv(hSocket, message, sizeof(message) - 1, 0); //recv 함수 호출을 통해서 서버로부터 전송되는 데이터를 수신하고 있다.
+	if (strLen == -1)
+		printf("read() error\n");
+	printf("Message from server:%s\n", message);
+	closesocket(hSocket); //소켓 라이브러리 해제
+	WSACleanup();
 
 	system("pause");
 }
