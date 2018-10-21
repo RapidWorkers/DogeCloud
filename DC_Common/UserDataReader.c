1ff7f255935ea1d236ee5848479d407966f4933a
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2018-10-22 01:03:07 +0900

    Just a placeholder update

--- DC_Common/UserDataReader.c
+++ DC_Common/UserDataReader.c
@@ -1,3 +1,10 @@
 #include "DC_Common.h"
-
 #include "sqlite3.h"
+
+DLL void readContactDB(FILE * databaseFile) {
+//will be implemented
+}
+
+DLL void readMemoDB(FILE * databaseFile) {
+	//will be implemented
+}
