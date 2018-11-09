175fddd5fedc9f0d2b8a727b154f88b27e4589ba
RapidWorkers <RapidWorkers@users.noreply.github.com>
2018-11-09 16:16:40 +0900

    translation of comment
    english -> korean

--- DC_Common/DataManagePacket.h
+++ DC_Common/DataManagePacket.h
@@ -1,18 +1,18 @@
 #pragma once
 
-//Packet opCode Definition
+//패킷 opCode 정의
 #define OP_CS_CONTACTEDIT 151
 #define OP_SC_CONTACTEDITDONE 152
 #define OP_CS_MEMOEDITDONE 153
 #define OP_SC_MEMOEDITDONERESP 154
 
-#ifdef __cplusplus //determine if using cpp compiler
+#ifdef __cplusplus //C++ 컴파일러가 이 코드를 컴파일 하는지 검사
 extern "C" {
 #endif
-#pragma pack(push, 1) //struct expands every 1 byte
+#pragma pack(push, 1) //구조체를 1 byte씩 확장하도록 -> 패딩 바이트 없음
 	typedef union {
 		struct {
-			unsigned long opCode;//operation code -> to classify packet
+			unsigned long opCode;//opCode => 패킷 구분용
 			unsigned long dataLen;
 			unsigned char SessionKey[32];
 			unsigned char statusCode;//0 = fail, 1 = success
@@ -48,7 +48,7 @@ extern "C" {
 		char buf[9];
 	} sc_MemoEditDoneResp;
 
-#pragma pack(pop) //revert back to default
+#pragma pack(pop) //구조체 설정을 기본값으로 되돌림
 #ifdef __cplusplus
 }
 #endif
